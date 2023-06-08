279bbb929337122a282fa82ff962132179ff411d
shechin <shechinmihail@mail.ru>
2023-06-08 21:25:42 +0500

    Добавил entity Ads, User, Image.

--- src/main/java/ru/skypro/homework/entity/AdsEntity.java
+++ src/main/java/ru/skypro/homework/entity/AdsEntity.java
@@ -17,7 +17,7 @@ public class AdsEntity {
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     /**
      * Описание объявления
