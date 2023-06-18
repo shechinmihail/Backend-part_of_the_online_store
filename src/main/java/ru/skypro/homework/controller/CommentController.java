42ab653d33c8e8dea5b6cb05e341deb52a4931ba
crushjunior <charushnikovvadim@gmail.con>
2023-06-18 11:15:22 +0300

    убрал api комментарии

--- src/main/java/ru/skypro/homework/controller/CommentController.java
+++ src/main/java/ru/skypro/homework/controller/CommentController.java
@@ -1,12 +1,7 @@
 package ru.skypro.homework.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.dto.Comment;
@@ -25,138 +20,22 @@ import ru.skypro.homework.dto.CreateComment;
 @RequestMapping("ads")
 public class CommentController {
 
-    /**
-     * Получить комментарии объявления
-     * @param id
-     * @return
-     */
-    @Operation(
-            summary = "Получить комментарии объявления",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "ОК",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Integer.class)
-                            )
-                    ),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Integer.class))
-                    )
-            }
-    )
     @GetMapping("{id}/comments")
     public ResponseEntity<?> getComments (@PathVariable int id) {
         return ResponseEntity.ok().build();
     }
 
-    /**
-     * Добавить комментарий к объявлению
-     * @param id
-     * @return
-     */
-    @Operation(
-            summary = "Добавить комментарий к объявлению",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "ОК",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Integer.class)
-                            )
-                    ),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Integer.class))
-                    )
-            }
-    )
     @PostMapping("{id}/comments")
     public ResponseEntity<?> addComment (@PathVariable int id, @RequestBody CreateComment createComment) {
         return ResponseEntity.ok().build();
     }
 
-    /**
-     * Удалить комментарий
-     * @param adId, commentId
-     * @return
-     */
-    @Operation(
-            summary = "Удалить комментарий",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "ОК",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Integer.class)
-                            )
-                    ),
-                    @ApiResponse(
-                            responseCode = "403",
-                            description = "Forbidden",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Integer.class)
-                            )
-                    ),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Integer.class))
-                    )
-            }
-    )
     @DeleteMapping("{adId}/comments/{commentId}")
     public ResponseEntity<?> deleteComment (@PathVariable int adId,
                                                @PathVariable int commentId) {
         return ResponseEntity.ok().build();
     }
 
-    /**
-     * Обновить комментарий
-     * @param adId, commentId
-     * @return
-     */
-    @Operation(
-            summary = "Обновить комментарий",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "ОК",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Integer.class)
-                            )
-                    ),
-                    @ApiResponse(
-                            responseCode = "403",
-                            description = "Forbidden",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Integer.class)
-                            )
-                    ),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Integer.class))
-                    )
-            }
-    )
     @PatchMapping("{adId}/comments/{commentId}")
     public ResponseEntity<?> updateComment (@PathVariable int adId,
                                          @PathVariable int commentId,
--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -1,9 +1,5 @@
 package ru.skypro.homework.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -28,134 +24,21 @@ import ru.skypro.homework.dto.User;
 @RequestMapping("users")
 public class UserController {
 
-
-    /**
-     * Обновление пароля
-     *
-     * @param newPassword новый пароль
-     * @return
-     */
-    @Operation(
-            summary = "Обновление пароля",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "ОК",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = NewPassword.class)
-                            )
-                    ),
-                    @ApiResponse(
-                            responseCode = "403",
-                            description = "Forbidden",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = NewPassword.class)
-                            )
-                    ),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = NewPassword.class))
-                    )
-            }
-    )
     @PostMapping("/set_password")
     public ResponseEntity<?> setPassword(@RequestBody NewPassword newPassword) {
         return ResponseEntity.ok().build();
     }
 
-    /**
-     * Получение информации об авторизованном пользователе
-     *
-     * @param authentication
-     * @return
-     */
-    @Operation(
-            summary = "Получить информацию об авторизованном пользователе пароля",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "ОК",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Authentication.class)
-                            )
-                    ),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = Authentication.class))
-                    )
-            }
-    )
     @GetMapping("/me")
     public ResponseEntity<?> getUser(@RequestBody Authentication authentication) {
         return ResponseEntity.ok().build();
     }
 
-    /**
-     * Обновить информацию об авторизованном пользователе
-     *
-     * @param user
-     * @return
-     */
-    @Operation(
-            summary = "Обновить информацию об авторизованном пользователе",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "ОК",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = User.class)
-                            )
-                    ),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = User.class))
-                    )
-            }
-    )
     @PatchMapping("/me")
     public ResponseEntity<?> updateUser(@RequestBody User user) {
         return ResponseEntity.ok().build();
     }
 
-    /**
-     * Обновить аватар авторизованного пользователя
-     *
-     * @param image
-     * @return
-     */
-    @Operation(
-            summary = "Обновить аватар авторизованного пользователя",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "ОК",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = MultipartFile.class)
-                            )
-                    ),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = MultipartFile.class))
-                    )
-            }
-    )
     @PatchMapping(value = "/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> updateUserImage(@RequestPart MultipartFile image) {
         return ResponseEntity.ok().build();
